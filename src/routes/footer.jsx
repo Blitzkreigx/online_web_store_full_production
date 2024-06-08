38f3b50ee544c78526f29ed44403121e3c1c6f22
Cristian Bautista Pérez <crhlstl4nk09@gmail.com>
2024-06-08 16:46:09 -0600

    Se desarrollo una tienda online

---
+++ src/routes/footer.jsx
@@ -0,0 +1,11 @@
+function Footer() {
+    return (
+        <>
+            <footer className="flex justify-center items-center p-5">
+                <small className="cursor-default">Hecho por <a href="https://github.com/Chrlssstlanx/" target="_blank"><b>chrlssstlanx</b></a></small>
+            </footer>
+        </>
+    )
+}
+
+export default Footer
